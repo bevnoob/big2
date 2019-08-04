50fa9198a3be4cbf8f7a37c423c0d2bf782fd980
Bevan Lee <bevan.lee@live.com.au>
2019-08-04 15:56:57 +1000

    init

---
+++ big2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Board from './Components/Board';
+
+
+function App() {
+  return (
+    <div className="App">
+      This is the big2
+      <div>
+        
+        <Board/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
